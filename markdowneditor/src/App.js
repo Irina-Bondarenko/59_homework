51856112c233163efa2771842f0181c108643b89
Irina-Bondarenko <96590953+Irina-Bondarenko@users.noreply.github.com>
2022-10-18 19:54:33 +0300

    init

---
+++ markdowneditor/src/App.js
@@ -0,0 +1,11 @@
+import { MarkdownEditorRender } from "./components/MarkdownEditorRender";
+
+function App() {
+  return (
+    <div className="App">
+      <MarkdownEditorRender />
+    </div>
+  );
+}
+
+export default App;
